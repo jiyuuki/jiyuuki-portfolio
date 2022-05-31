ac655fa934314520e5d58b0383987a8bc0645a04
Salma EL HAJRAOUI <salma.hajrawi@gmail.com>
2022-05-31 17:56:02 +0200

    oppps

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+<script>
+import { onMounted } from 'vue'
+export default {
+  name: 'App',
+
+  setup () {
+    onMounted(() => {
+      import('@/assets/icons/dark.svg?sprite').then((response) => {
+        console.log({ response })
+      }).catch((error) => {
+        console.log({ error })
+      })
+    })
+  }
+}
+</script>
+<style></style>
---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg')
+
+    svgRule.uses.clear()
+
+    svgRule
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+  }
+})
